6806027d720bcbed785f998147d74e8f2fb098dd
czarjulius <juliusczar.jc@gmail.com>
2023-10-11 15:18:28 +0100

    render nested comments

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,5 +1,6 @@
+import { CommentService } from './../../services/comment.service';
 import { CommentFormComponent } from './../comment-form/comment-form.component';
-import { Component, Input } from '@angular/core';
+import { Component, Input, effect, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Comment } from '../../interfaces/comment.interface';
 
@@ -12,16 +13,26 @@ import { Comment } from '../../interfaces/comment.interface';
 })
 export class CommentComponent {
   @Input() comment!: Comment;
-  isExpanded = false;
-  isReplying = false;
+  isExpanded = signal(false);
+  isReplying = signal(false);
+  commentService = inject(CommentService);
+  nestedComments = signal<Comment[]>([]);
+
+  nestedCommentsEffect = effect(() => {
+    if (this.isExpanded()) {
+      this.commentService.getComment(this.comment._id).subscribe((comments) => {
+        this.nestedComments.set(comments);
+      });
+    }
+  });
 
   toggleReplying() {
-    this.isReplying = !this.isReplying;
-    if (this.isReplying) {
-      this.isExpanded = true;
+    this.isReplying.set(!this.isReplying());
+    if (this.isReplying()) {
+      this.isExpanded.set(true);
     }
   }
   toggleExpanded() {
-    this.isExpanded = !this.isExpanded;
+    this.isExpanded.set(!this.isExpanded());
   }
 }
